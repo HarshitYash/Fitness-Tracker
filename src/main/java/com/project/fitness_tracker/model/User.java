d0150414b1ff7da9c667c21d02dcec88ad1901a8
HarshitYash <harshitsahu616@gmail.com>
2026-07-29 12:54:01 +0530

    Set up Fitness Tracker backend with Spring Boot and MySQL

---
+++ src/main/java/com/project/fitness_tracker/model/User.java
@@ -0,0 +1,31 @@
+package com.project.fitness_tracker.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Activity> activities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Recommendation> recommendations = new ArrayList<>();
+}
